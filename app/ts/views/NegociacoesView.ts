5c183bb7372158e33c08fe848493c0ecdb6aaa4d
Barbosa <nicolas.a.barbosa@accenture.com>
2018-03-20 16:48:15 -0300

    ts 1.3.5

--- app/ts/views/NegociacoesView.ts
+++ app/ts/views/NegociacoesView.ts
@@ -6,11 +6,11 @@ class NegociacoesView {
         this._elemento = document.querySelector(seletor)
     }
     
-    update(): void {
-        this._elemento.innerHTML = this.template()
+    update(model: Negociacoes): void {
+        this._elemento.innerHTML = this.template(model)
     }
 
-    template(): string {
+    template(model: Negociacoes): string {
 
         return `
             <table class="table table-hover table-bordered">
@@ -24,11 +24,23 @@ class NegociacoesView {
                 </thead>
 
                 <tbody>
+                    ${model
+                        .paraArray()
+                        .map(negociacao => 
+                            `
+                                <tr>
+                                    <td>${negociacao.data.getDate()}/${negociacao.data.getMonth() +1}/${negociacao.data.getFullYear()}</td>
+                                    <td>${negociacao.quantidade}</td>
+                                    <td>${negociacao.valor}</td>
+                                    <td>${negociacao.volume}</td>
+                                </tr>
+                            `
+                        ).join('')
+                    }
                 </tbody>
-
                 <tfoot>
                 </tfoot>
             </table>          
         `
     }
-}
+}   
