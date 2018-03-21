30ad91d8161ef034a809e8068733b752c474d87f
Barbosa <nicolas.a.barbosa@accenture.com>
2018-03-21 11:49:06 -0300

    ts 1.4.4

--- app/ts/views/View.ts
+++ app/ts/views/View.ts
@@ -1,7 +1,16 @@
-class View {
+class View<T> {
+
     protected _elemento: Element
 
     constructor(seletor: string) {
         this._elemento = document.querySelector(seletor)
     }
+
+    update(model: T): void {
+        this._elemento.innerHTML = this.template(model)
+    }
+
+    template(model: T): string {
+        throw new Error('Você deve sobrescrever o método template')
+    }   
 }
