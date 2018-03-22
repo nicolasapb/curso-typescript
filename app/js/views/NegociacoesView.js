bab32fb4f1cdaacde9a67f603906f8439a6b86eb
nicolasapb <nicolas.a.p.barbosa@gmail.com>
2018-03-22 15:18:21 -0300

    ts 2.1.3

--- app/js/views/NegociacoesView.js
+++ app/js/views/NegociacoesView.js
@@ -1,35 +1,32 @@
-var Views;
-(function (Views) {
-    class NegociacoesView extends Views.View {
-        template(model) {
-            return `
-            <table class="table table-hover table-bordered">
-                <thead>
-                    <tr>
-                        <th>DATA</th>
-                        <th>QUANTIDADE</th>
-                        <th>VALOR</th>
-                        <th>VOLUME</th>
-                    </tr>
-                </thead>
+import { View } from "./View";
+export class NegociacoesView extends View {
+    template(model) {
+        return `
+        <table class="table table-hover table-bordered">
+            <thead>
+                <tr>
+                    <th>DATA</th>
+                    <th>QUANTIDADE</th>
+                    <th>VALOR</th>
+                    <th>VOLUME</th>
+                </tr>
+            </thead>
 
-                <tbody>
-                    ${model
-                .paraArray()
-                .map(negociacao => `
-                                <tr>
-                                    <td>${negociacao.data.getDate()}/${negociacao.data.getMonth() + 1}/${negociacao.data.getFullYear()}</td>
-                                    <td>${negociacao.quantidade}</td>
-                                    <td>${negociacao.valor}</td>
-                                    <td>${negociacao.volume}</td>
-                                </tr>
-                            `).join('')}
-                </tbody>
-                <tfoot>
-                </tfoot>
-            </table>          
-        `;
-        }
+            <tbody>
+                ${model
+            .paraArray()
+            .map(negociacao => `
+                            <tr>
+                                <td>${negociacao.data.getDate()}/${negociacao.data.getMonth() + 1}/${negociacao.data.getFullYear()}</td>
+                                <td>${negociacao.quantidade}</td>
+                                <td>${negociacao.valor}</td>
+                                <td>${negociacao.volume}</td>
+                            </tr>
+                        `).join('')}
+            </tbody>
+            <tfoot>
+            </tfoot>
+        </table>          
+    `;
     }
-    Views.NegociacoesView = NegociacoesView;
-})(Views || (Views = {}));
+}
--- app/ts/models/Negociacao.ts
+++ app/ts/models/Negociacao.ts
@@ -1,4 +1,5 @@
-class Negociacao { 
+
+export class Negociacao { 
 
     constructor(private _data: Date, private _quantidade: number, private _valor: number) {}
 
